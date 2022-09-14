cec339f64ba7db7db01fdd8b8b3724c63a518247
Leandro <perezbarahonaleandro@gmail.com>
2022-09-14 18:23:52 -0400

    beta desktop navbar styles added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -25,6 +25,7 @@ export default function NavBar(){
             <div className='right'>
                 <div className='add-btn'>
                     <svg width='12' height='12' xmlns='http://www.w3.org/2000/svg'><path fill='#FFF' d='M7.368 12V7.344H12V4.632H7.368V0H4.656v4.632H0v2.712h4.656V12z'/></svg>
+                    <span>Add New Task</span>
                 </div>
                 <div className='more'>
                     <svg width='5' height='20' xmlns='http://www.w3.org/2000/svg'><g fill='#828FA3' fillRule='evenodd'><circle cx='2.308' cy='2.308' r='2.308'/><circle cx='2.308' cy='10' r='2.308'/><circle cx='2.308' cy='17.692' r='2.308'/></g></svg>
@@ -33,16 +34,18 @@ export default function NavBar(){
 
             
             <div className={`burger-menu ${burgerMenuState}`}>
-                <h3>ALL BOARDS (0)</h3>
-                <ul>
-                    <li>
+                <div className="content">
+                    <h3>ALL BOARDS (0)</h3>
+                    <ul>
+                        <li>
+                            <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z" fill="#828FA3"/></svg>
+                            <span>Platform Launch</span>
+                        </li>
+                    </ul>
+                    <div className='create-board'>
                         <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z" fill="#828FA3"/></svg>
-                        <span>Platform Launch</span>
-                    </li>
-                </ul>
-                <div className='create-board'>
-                    <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z" fill="#828FA3"/></svg>
-                    <span>+ Create New Board</span>
+                        <span>+ Create New Board</span>
+                    </div>
                 </div>
                 <div className='theme-changer'>
                     <svg width="19" height="19" xmlns="http://www.w3.org/2000/svg"><path d="M9.167 15.833a.833.833 0 0 1 .833.834v.833a.833.833 0 0 1-1.667 0v-.833a.833.833 0 0 1 .834-.834ZM3.75 13.75a.833.833 0 0 1 .59 1.422l-1.25 1.25a.833.833 0 0 1-1.18-1.178l1.25-1.25a.833.833 0 0 1 .59-.244Zm10.833 0c.221 0 .433.088.59.244l1.25 1.25a.833.833 0 0 1-1.179 1.178l-1.25-1.25a.833.833 0 0 1 .59-1.422ZM9.167 5a4.167 4.167 0 1 1 0 8.334 4.167 4.167 0 0 1 0-8.334Zm-7.5 3.333a.833.833 0 0 1 0 1.667H.833a.833.833 0 1 1 0-1.667h.834Zm15.833 0a.833.833 0 0 1 0 1.667h-.833a.833.833 0 0 1 0-1.667h.833Zm-1.667-6.666a.833.833 0 0 1 .59 1.422l-1.25 1.25a.833.833 0 1 1-1.179-1.178l1.25-1.25a.833.833 0 0 1 .59-.244Zm-13.333 0c.221 0 .433.088.59.244l1.25 1.25a.833.833 0 0 1-1.18 1.178L1.91 3.09a.833.833 0 0 1 .59-1.422ZM9.167 0A.833.833 0 0 1 10 .833v.834a.833.833 0 1 1-1.667 0V.833A.833.833 0 0 1 9.167 0Z" fill="#828FA3"/></svg>
@@ -66,13 +69,24 @@ const Wrapper = styled.div`
     align-items: center;
     padding: 0 10px;
     justify-content: space-between;
+    @media (min-width: 768px) {
+        height: 5em;
+        padding: 0 15px;
+    }
    .left{
     display: flex;
+    align-items: center;
         .logo{
             display: flex;
             align-items: center;
+            z-index: 201;
             h1{
                 display: none;
+                @media (min-width: 768px) {
+                    display: block;
+                    margin-left: 10px;
+                    color: #fff;
+                }
             }
         }
         .burger-menu-toggle{
@@ -80,9 +94,26 @@ const Wrapper = styled.div`
             align-items: center;
             margin-left: 10px;
             color: #fff;
+            @media (min-width: 768px) {
+                pointer-events: none;
+                margin-left: 100px;
+            }
             h2{
                 font-size: 16px;
                 margin-right: 10px;
+            }
+            svg{
+                @media (min-width: 768px) {
+                    display: none;
+                }
+                &.visible{
+                    animation: rotate .3s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s 1 normal forwards;
+                    @keyframes rotate {100%{transform: rotate(180deg);}}
+                }
+                &.hidden{
+                    animation: rotateBack .3s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s 1 normal forwards;
+                    @keyframes rotateBack {100%{transform: rotate(360deg);}}
+                }
             }
         }
    }
@@ -91,13 +122,33 @@ const Wrapper = styled.div`
         align-items: center;
         .add-btn{
             background-color: #635fc7;
-            width: 3em;
+            min-width: 3em;
             height: 2em;
+            width: fit-content;
             display: flex;
             justify-content: center;
             align-items: center;
             border-radius: 15px;
             margin-right: 10px;
+            padding: 0 25px;
+            @media (min-width: 768px){
+                height: 2.5em;
+                border-radius: 25px;
+                margin-right: 20px;
+            }
+            svg{
+                @media (min-width: 768px) {
+                    transform: scale(0.75);
+                }
+            }
+            span{
+                display: none;
+                @media  (min-width: 768px){
+                    display: block;
+                    margin-left: 5px;
+                    color: #fff;
+                }
+            }
         }
    }
    .bg{
@@ -125,8 +176,28 @@ const Wrapper = styled.div`
         border-radius: 5px;
         padding: 15px 20px;
         z-index: 200;
+        @media (min-width: 768px) {
+            top: 0;
+            left: 0;
+            width: 15em;
+            height: 100vh;
+            border-radius: 0;
+            background-color: #2b2c37;
+            border-right: 1px solid #b3adad99;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-between;
+            padding: 15px;
+            .content{
+                margin-top: 100px;
+                width: 100%;
+            }
+        }
         &.null{
             display: none;
+            @media (min-width: 768px) {
+                display: flex;
+            }
         }
         &.visible{
             transform: scale(0);
@@ -179,6 +250,9 @@ const Wrapper = styled.div`
             justify-content: space-evenly;
             height: 2.5em;
             margin-top: 15px;
+            @media (min-width: 768px) {
+                background-color: #20212c;
+            }
             div{
                 background-color: #635fc7;
                 width: 40px;
