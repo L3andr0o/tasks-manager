81a8dbb54fe1df941013b78cabd4b1e63d11479a
Leandro <perezbarahonaleandro@gmail.com>
2022-09-19 16:03:05 -0400

    edit board modal mobile ready

---
+++ src/components/editBoardModal.tsx
@@ -0,0 +1,122 @@
+import styled from "styled-components"
+
+export default function EditBoardModal(){
+    return(
+        <Wrapper>
+            <div className="modal">
+                <h1>Edit Board</h1>
+                <label htmlFor="board-name">Board Name</label>
+                <input type="text" value='Plataform Launch' id="board-name" />
+
+                <form className="board-columns">
+                    <span>Board Columns</span>
+                    <div>
+                        <input type="text" value='Todo' />
+                        <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg"><g fill="#828FA3" fill-rule="evenodd"><path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z"/><path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z"/></g></svg>
+                    </div>
+                    <div>
+                        <input type="text" value='Doing' />
+                        <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg"><g fill="#828FA3" fill-rule="evenodd"><path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z"/><path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z"/></g></svg>
+                    </div><div>
+                        <input type="text" value='Done' />
+                        <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg"><g fill="#828FA3" fill-rule="evenodd"><path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z"/><path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z"/></g></svg>
+                    </div>
+                    <div className="buttons">
+                        <button className="add-new-column">+ Add New Column</button>
+                        <button className="save-changes">Save Changes</button>
+                    </div>
+                </form>
+
+            </div>
+        </Wrapper>
+    )
+}
+
+
+const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color:#0003;
+    position: absolute;
+    left: 0;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    .modal{
+        width: 90%;
+        background-color: #2b2c37;
+        padding: 25px 20px;
+        color: #fff;
+        border-radius: 5px;
+        max-width: 25em;
+        animation: show .3s cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s 1 normal forwards;
+        transform: scale(0);
+        z-index: 200;
+        @keyframes show {
+            100%{transform:scale(1)}
+        }
+        input{
+            background-color: transparent;
+            border: 1px solid #ffffff28;
+            color: #fff;
+            border-radius: 5px;
+            padding: 10px;
+        }
+        h1{
+            font-size: 18px;
+            font-weight: 500;
+            margin-bottom: 15px;
+        }
+        label{
+            display: block;
+            font-size: 12px;
+            font-weight: 600;
+            margin-bottom: 5px;
+        }
+        #board-name{
+            width: 100%;
+        }
+        form{
+            margin-top: 15px;
+            span{
+                font-size: 12px;
+                font-weight: 600;
+            }
+            div{
+                margin-top: 10px;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                input{
+                    width: 90%;
+                }
+            }
+            .buttons{
+                display: flex;
+                flex-direction: column;
+                width: 100%;
+                button{
+                    width: 100%;
+                    padding: 10px;
+                    border-radius: 25px;
+                    border: none;
+                    outline: none;
+                    font-weight: 600;
+                }
+                .add-new-column{
+                    background-color: #fff;
+                    color: #635fc7;
+                    
+                }
+                .save-changes{
+                    background-color: #635fc7;
+                    color: #fff;
+                    margin-top: 15px;
+                }
+            }
+        }
+    }
+
+`
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { GlobalStyles } from '../assets/globalStyles/globalStyles';
+import EditBoardModal from '../components/editBoardModal';
 import NavBar from '../components/navbar';
 
 export default function Home(){
+
+    const [editBoardModal, setEditBoardModal] = useState<boolean>(false);
+
+
     return(
         <Wrapper>
             <GlobalStyles />
             <NavBar />
-
+                <div className="content">
+                    <div className="empty">
+                        <h1>This board is empty. Create a new column to get started.</h1>
+                        <button onClick={()=> setEditBoardModal(true)}>+ Add New Column</button>
+                    </div>
+                </div>
+                {
+                    (editBoardModal) && <EditBoardModal />
+                }
         </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
     position: relative;
+    background-color: #20212c;
+    min-height: 100vh;
+    height: 100%;
+    .content{
+        height: 90vh;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        /* background-color: #000; */
+        .empty{
+        margin: 0 auto;
+        width: 80%;
+        max-width: 30em;
+        text-align: center;
+        background-color: #20212c;
+        
+        h1{
+            font-size: 18px;
+            color: #9292af;
+        }
+        button{
+            background-color: #635fc7;
+            border: none;
+            outline: none;
+            color: #fff;
+            padding: 15px;
+            border-radius: 25px;
+            font-size: 16px;
+            font-weight: 600;
+            margin-top: 15px;
+        }
+    }
+    }
 `
